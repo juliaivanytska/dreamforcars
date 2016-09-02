c01c25f94e6af37923594094da9b3f0ebe4306a6
Yuliya <juliaivanytska15@gmail.com>
2016-09-02 20:07:07 +0000

    gh-pages

---
+++ js/index.js
@@ -0,0 +1,65 @@
+.header {position:fixed;
+         height:150px;
+         width:100%;
+         background:url(header.jpg);
+         opacity: 0.8;
+         margin:0 auto;    
+         left: 0;
+         top: 0;
+         z-index: 120;
+         direction: ltr;
+}
+.nav { width: 16%;
+    display: inline-block;
+     box-shadow: 1px 1px 3px #999999;}
+.main {
+    background:url(b3.jpg);
+    margin:0 auto;
+    width: 100%;
+    height: 800px;
+}
+.auto{background:url(fo.jpg) no-repeat;
+     opacity:0.9;
+margin: 0 auto;
+    width: 100%;
+    height: 800px;
+   
+}
+.autoserv{background:url(serv.jpg) no-repeat;
+    margin: 0 auto;
+    width:100%;
+    height:800px;
+}
+.onlineform{background:url(b3.jpg)no-repeat;
+     width:100%;
+     height:800px;
+}
+#fontt{color: white;
+    font-size:20px;
+    
+}
+.about{background:url(about.jpg) no-repeat;
+    width:100%;
+    height:800px;
+    font:solid white;
+}
+.contact{width:1350px;
+    height:800px;
+    background:url()no-repeat;
+}
+
+h1{color:#fffafa;
+  font-size:24px;
+  font-style:normal;
+  
+}
+.textabout{text-align:center;
+    vertical-align: middle;
+    color:white;
+    font-size:36px;
+    font-family: Arial serif;
+}
+
+.footer {clear: both;
+    
+}
